19918fe137424a1b6d1a987983e8f994b6650f11
逆流而上 <1666888816@qq.com>
2023-10-19 10:02:49 +0800

    feat: flow rulus store nacos

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/IdSettingAspect.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/IdSettingAspect.java
@@ -1,21 +1,40 @@
 package com.alibaba.csp.sentinel.dashboard.controller.v2;
 
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.RuleEntity;
-import com.alibaba.csp.sentinel.dashboard.repository.rule.InMemoryRuleRepositoryAdapter;
+import com.alibaba.csp.sentinel.dashboard.rule.nacos.NacosConfigUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Aspect
 @Component
 public class IdSettingAspect {
+    @Autowired
+    private ConfigService configService;
+
+    private Long getAndIncrementId() throws Exception {
+        String id = configService.getConfig("id", NacosConfigUtil.GROUP_ID, 3000);
+        AtomicLong ids = new AtomicLong(Long.parseLong(id));
+        long newId = ids.incrementAndGet();
+        configService.publishConfig("id", NacosConfigUtil.GROUP_ID, String.valueOf(newId));
+        return newId;
+    }
+
+
+    //全局共用一个自增id，就不需要对每个规则类型都设置一个自增id了
     @Before("execution(* com.alibaba.csp.sentinel.dashboard.repository.rule.InMemoryRuleRepositoryAdapter.save(..)) && args(entity)")
     public void setIdIfNull(RuleEntity entity) {
 
         if (entity.getId() == null) {
-            System.out.println("设置id");
-            // 在这里设置id的值，可以调用自定义的方法
-//            entity.setId(customNextIdLogic());
+            try {
+                entity.setId(getAndIncrementId());
+            } catch (Exception ignored) {
+            }
         }
     }
 
