ce0dabd1190bec8e59f233456070b8e594d1d290
逆流而上 <1666888816@qq.com>
2023-10-25 10:58:55 +0800

    feat: add ReportController

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/report/ReportController.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/report/ReportController.java
@@ -1,6 +1,7 @@
 package com.alibaba.csp.sentinel.dashboard.controller.v2.report;
 
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.MetricEntity;
+import com.alibaba.csp.sentinel.dashboard.domain.vo.MetricVo;
 import com.alibaba.csp.sentinel.dashboard.repository.metric.MetricsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController
 @RequestMapping(value = "/sentinel-service/report")
@@ -15,17 +18,37 @@ public class ReportController {
     @Autowired
     private MetricsRepository<MetricEntity> metricStore;
 
-
+    private Iterable<MetricVo> sortMetricVoAndDistinct(List<MetricVo> vos) {
+        if (vos == null) {
+            return null;
+        }
+        Map<Long, MetricVo> map = new TreeMap<>();
+        for (MetricVo vo : vos) {
+            MetricVo oldVo = map.get(vo.getTimestamp());
+            if (oldVo == null || vo.getGmtCreate() > oldVo.getGmtCreate()) {
+                map.put(vo.getTimestamp(), vo);
+            }
+        }
+        return map.values();
+    }
     @Scheduled(fixedRate = 5000) // 每隔5秒执行一次
     public void test() {
         long endTime = System.currentTimeMillis();
         long startTime = endTime - 1000 * 10;
         String app = "graee68d";
-        List<MetricEntity> entities = metricStore.queryByAppAndResourceBetween(app, "/pc-service/api/hello", startTime, endTime);
+        String resource = "/pc-service/api/hello";
+        List<MetricEntity> entities = metricStore.queryByAppAndResourceBetween(app,resource , startTime, endTime);
 
-        long sumSuccessQps = entities.stream()
-                .mapToLong(MetricEntity::getPassQps)
-                .sum();
+        List<MetricVo> vos = MetricVo.fromMetricEntities(entities, resource);
+        Iterable<MetricVo> vosSorted = sortMetricVoAndDistinct(vos);
+
+        int sumSuccessQps = 0;
+        for (MetricVo metricVo : vosSorted) {
+            sumSuccessQps+=metricVo.getSuccessQps();
+        }
+//        long sumSuccessQps = entities.stream()
+//                .mapToLong(MetricEntity::getPassQps)
+//                .sum();
         System.out.println("sumSuccessQps: " + sumSuccessQps);
         System.out.println("entities: " + entities.size());
     }
--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/report/ReportEntity.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/report/ReportEntity.java
@@ -3,7 +3,9 @@ package com.alibaba.csp.sentinel.dashboard.datasource.entity.report;
 public class ReportEntity {
     private int id;
 
-    private String app;
+    private String app;//组件名
 
-    private String resource;
+    private String resource;//资源名
+
+    private String ip;//ip地址
 }
